2b0f73568e6d538460c0fbd222b79c6cb29099c8
Daniel Stockman <daniel.stockman@gmail.com>
2018-04-05 12:10:19 -0700

    fix(react): Disable react/destructuring-assignment as it is too naïve to be useful

--- packages/eslint-config-zillow/rules/react.js
+++ packages/eslint-config-zillow/rules/react.js
@@ -415,7 +415,8 @@ module.exports = {
 
     // Enforce consistent usage of destructuring assignment of props, state, and context
     // https://github.com/yannickcr/eslint-plugin-react/blob/843d71a432baf0f01f598d7cf1eea75ad6896e4b/docs/rules/destructuring-assignment.md
-    'react/destructuring-assignment': ['error', 'always'],
+    // TODO: enable when it is less naïve about shadowing outer scopes
+    'react/destructuring-assignment': ['off', 'always'],
 
     // Prevent using this.state within a this.setState
     // https://github.com/yannickcr/eslint-plugin-react/blob/843d71a432baf0f01f598d7cf1eea75ad6896e4b/docs/rules/no-access-state-in-setstate.md
