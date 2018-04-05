2a771197790ce41db51fc6c080c2df469980a681
Daniel Stockman <daniel.stockman@gmail.com>
2018-04-04 17:44:42 -0700

    chore(babel-preset-zillow): Reformat with prettier

--- packages/babel-preset-zillow/index.js
+++ packages/babel-preset-zillow/index.js
@@ -1,62 +1,59 @@
 'use strict';
 
-/* eslint indent: [error, 2], global-require: off */
-
-const modules = [require('babel-plugin-transform-es2015-modules-commonjs'), {
-  strict: false,
-}];
-
 const defaultTargets = {
-  android: 62,
-  chrome: 62,
-  edge: 15,
-  explorer: 11,
-  firefox: 57,
-  safari: 9,
+    android: 62,
+    chrome: 62,
+    edge: 15,
+    explorer: 11,
+    firefox: 57,
+    safari: 9,
 };
 
 function buildTargets(options) {
-  return Object.assign({}, defaultTargets, options.additionalTargets);
+    return Object.assign({}, defaultTargets, options.additionalTargets);
 }
 
 module.exports = function babelPresetZillow(context, options) {
-  const envIsDev = (process.env.BABEL_ENV || process.env.NODE_ENV || 'development') === 'development';
-  const transpileTargets = (options && options.targets) || buildTargets(options);
-  const enableDebug = (options && typeof options.debug === 'boolean') ? !!options.debug : false;
+    const envIsDev =
+        (process.env.BABEL_ENV || process.env.NODE_ENV || 'development') === 'development';
+    const targets = (options && options.targets) || buildTargets(options);
+    const debug = options && typeof options.debug === 'boolean' ? !!options.debug : false;
 
-  return {
-    presets: [
-      [require('babel-preset-env'), {
-        debug: enableDebug,
-        exclude: [
-          'transform-async-to-generator',
-          'transform-es2015-block-scoping',
-          'transform-es2015-template-literals',
-          'transform-regenerator',
+    /* eslint global-require: off */
+    const presets = [
+        [
+            require('babel-preset-env'),
+            {
+                debug,
+                exclude: [
+                    'transform-async-to-generator',
+                    'transform-es2015-block-scoping',
+                    'transform-es2015-template-literals',
+                    'transform-regenerator',
+                ],
+                modules: false,
+                targets,
+            },
         ],
-        modules: false,
-        targets: transpileTargets,
-      }],
-      require('babel-preset-react'),
-    ],
-    plugins: [
-      options && options.modules === false ? null : modules,
-      require('babel-plugin-transform-class-properties'),
-      [require('fast-async'), {
-        spec: true,
-      }],
-      [require('babel-plugin-transform-es2015-block-scoping'), {
-        throwIfClosureRequired: true,
-      }],
-      [require('babel-plugin-transform-es2015-template-literals'), {
-        spec: true,
-      }],
-      [require('babel-plugin-transform-object-rest-spread'), {
-        useBuiltIns: true,
-      }],
-      require('babel-plugin-lodash'),
-      envIsDev && require('babel-plugin-transform-react-jsx-source'),
-      envIsDev && require('babel-plugin-transform-react-jsx-self'),
-    ].filter(Boolean),
-  };
+        require('babel-preset-react'),
+    ];
+
+    const plugins = [
+        options && options.modules === false
+            ? null
+            : [require('babel-plugin-transform-es2015-modules-commonjs'), { strict: false }],
+        require('babel-plugin-transform-class-properties'),
+        [require('fast-async'), { spec: true }],
+        [require('babel-plugin-transform-es2015-block-scoping'), { throwIfClosureRequired: true }],
+        [require('babel-plugin-transform-es2015-template-literals'), { spec: true }],
+        [require('babel-plugin-transform-object-rest-spread'), { useBuiltIns: true }],
+        require('babel-plugin-lodash'),
+        envIsDev && require('babel-plugin-transform-react-jsx-source'),
+        envIsDev && require('babel-plugin-transform-react-jsx-self'),
+    ].filter(Boolean);
+
+    return {
+        presets,
+        plugins,
+    };
 };
