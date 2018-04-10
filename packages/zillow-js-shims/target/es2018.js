4a785a4ecbab37261eb09b51ae9af4c32dc97e1b
Daniel Stockman <daniel.stockman@gmail.com>
2018-04-10 13:09:32 -0700

    feat: Fork airbnb-js-shims -> zillow-js-shims

--- packages/zillow-js-shims/target/es2018.js
+++ packages/zillow-js-shims/target/es2018.js
@@ -2,7 +2,7 @@
 
 // Promise.prototype.finally is stage 3, a possible candidate for ES2018
 if (typeof Promise === 'function') {
-  require('promise.prototype.finally/shim')(); // eslint-disable-line global-require
+    require('promise.prototype.finally/shim')(); // eslint-disable-line global-require
 }
 
 require('array.prototype.flatten/shim')();
---
+++ packages/zillow-js-shims/test/zillow-js-shims.test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/* eslint-disable global-require, import/no-dynamic-require */
+
+const fs = require('fs');
+const path = require('path');
+
+test('main', () => {
+    expect(() => require('..')).not.toThrow();
+});
+
+describe('target', () => {
+    const targetsPath = path.join(__dirname, '../target');
+    const targets = fs.readdirSync(targetsPath);
+
+    targets.forEach(target => {
+        test(target, () => {
+            expect(() => require(path.join(targetsPath, target))).not.toThrow();
+        });
+    });
+});
