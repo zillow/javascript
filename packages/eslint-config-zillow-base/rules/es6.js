307647f898e798daf2e4ac204c280d24aa84fe2d
Daniel Stockman <daniel.stockman@gmail.com>
2018-04-26 12:12:17 -0700

    fix(base): Demote prefer-destructuring to a warning, as it is not auto-fixable

--- packages/eslint-config-zillow-base/rules/es6.js
+++ packages/eslint-config-zillow-base/rules/es6.js
@@ -114,7 +114,7 @@ module.exports = {
 
     // Prefer destructuring from arrays and objects
     // https://eslint.org/docs/rules/prefer-destructuring
-    'prefer-destructuring': ['error', {
+    'prefer-destructuring': ['warn', {
       VariableDeclarator: {
         array: false,
         object: true,
