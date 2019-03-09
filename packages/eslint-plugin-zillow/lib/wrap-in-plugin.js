8215981a692c65398a45115f8e1a1d90f1c34b30
Daniel Stockman <daniel.stockman@gmail.com>
2019-03-09 11:43:15 -0800

    refactor(plugin): Object.assign -> spread

--- packages/eslint-plugin-zillow/lib/wrap-in-plugin.js
+++ packages/eslint-plugin-zillow/lib/wrap-in-plugin.js
@@ -6,14 +6,16 @@ module.exports = wrapInPlugin;
 
 function wrapInPlugin(config, files) {
     // if files passed, this whole block will be moved to overrides[0]
-    const pluginConfig = Object.assign({}, { files }, config, {
+    const pluginConfig = {
+        files,
         // We expose a config already computed from the whole extends chain, so no extends here.
+        ...config,
         extends: [],
         // Plugins appear to come from this plugin, so it's the only one externally visible.
         plugins: ['zillow'],
         // The rules from third-party plugins need to be prefixed so they reference our namespace.
         rules: prefixRuleConfigs('zillow', config.rules),
-    });
+    };
 
     if (files) {
         // https://eslint.org/docs/user-guide/configuring#configuration-based-on-glob-patterns
